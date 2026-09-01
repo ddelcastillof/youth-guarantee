b22443fa53497aaf24d4130dee0d2b6168ce01a6
Darwin Del Castillo <ddelcast@uw.edu>
2026-09-01 10:46:48 +0100

    add: helper script

---
+++ slurm/download_summarised_output.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Utility script
+
+HPC_USER=dd198b
+HPC_HOST=mars-login.ice.gla.ac.uk
+HPC_DIR=/users/dd198b/Documents/GitHub
+
+scp -r $HPC_USER@$HPC_HOST:$HPC_DIR/youth-guarantee/data/simpaths_output/baseline/output_dirs.txt ./data/simpaths_output/baseline/output.dirs.txt
+scp -r $HPC_USER@$HPC_HOST:$HPC_DIR/youth-guarantee/data/simpaths_output/baseline/summarised_output.csv ./data/simpaths_output/baseline/summarised_output.csv
