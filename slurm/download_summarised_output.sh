ffbd1400cf8da9be339c6f98e197a1a18c5c9968
Darwin Del Castillo <ddelcast@uw.edu>
2026-09-02 16:32:48 +0100

    add: new scenario

--- slurm/download_summarised_output.sh
+++ slurm/download_summarised_output.sh
@@ -6,5 +6,8 @@ HPC_USER=dd198b
 HPC_HOST=mars-login.ice.gla.ac.uk
 HPC_DIR=/users/dd198b/Documents/GitHub
 
-scp -r $HPC_USER@$HPC_HOST:$HPC_DIR/youth-guarantee/data/simpaths_output/baseline/output_dirs.txt ./data/simpaths_output/baseline/output.dirs.txt
+scp -r $HPC_USER@$HPC_HOST:$HPC_DIR/youth-guarantee/data/simpaths_output/baseline/output_dirs.txt ./data/simpaths_output/baseline/output_dirs.txt
 scp -r $HPC_USER@$HPC_HOST:$HPC_DIR/youth-guarantee/data/simpaths_output/baseline/summarised_output.csv ./data/simpaths_output/baseline/summarised_output.csv
+
+scp -r $HPC_USER@$HPC_HOST:$HPC_DIR/youth-guarantee/data/simpaths_output/hi-only/output_dirs.txt ./data/simpaths_output/hi-only/output_dirs.txt
+scp -r $HPC_USER@$HPC_HOST:$HPC_DIR/youth-guarantee/data/simpaths_output/hi-only/summarised_output.csv ./data/simpaths_output/hi-only/summarised_output.csv
